dc22dd859745ee5ad51fa04fd656c7ea69da20a3
Thomas Goossens <thomas@drillster.com>
2026-01-31 13:50:30 +0100

    Improve hashCode and equals

--- lib/src/ansi_text.dart
+++ lib/src/ansi_text.dart
@@ -42,10 +42,30 @@ class AnsiText {
 
   @override
   // ignore: avoid_equals_and_hash_code_on_mutable_classes
-  bool operator ==(Object other) =>
-      other is AnsiText && other.toString() == toString();
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other is! AnsiText) {
+      return false;
+    }
+
+    return _text == other._text && _listEquals(_styles, other._styles);
+  }
 
   @override
   // ignore: avoid_equals_and_hash_code_on_mutable_classes
-  int get hashCode => toString().hashCode;
+  int get hashCode => Object.hash(_text, Object.hashAll(_styles));
+
+  bool _listEquals<T>(List<T> a, List<T> b) {
+    if (a.length != b.length) {
+      return false;
+    }
+    for (var i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
